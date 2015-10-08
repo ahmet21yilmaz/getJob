ff7a3c47fc2b15ffbbbabac6e4298c3d7a8c1893
ahmet21ylmz <ahmet21ylmz@gmail.com>
2015-10-08 21:09:16 +1100

    getjob

---
+++ www/js/app.js
@@ -0,0 +1,171 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform,$rootScope) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+
+/*
+    //stateChangeError
+    $rootScope.$on('$stateChangeError', function(event, toState,
+      toParams, fromState, fromParams, error) {
+      if (error === 'AUTH_REQUIRED') {
+      $state.go('main');
+      }
+    });  */
+  });
+})
+//you can attach multiple run methods to a module
+.run(['$rootScope', 'AuthFactory', function($rootScope,AuthFactory){
+    //creating a property on $rootScope object
+      $rootScope.isAuthenticated == AuthFactory.isLoggedIn();//using AuthFactory methods to find the status
+
+}])
+
+.config(function($stateProvider, $urlRouterProvider,$httpProvider,$ionicConfigProvider) {
+//we will use $ionicConfigProvider to set up a few defaults.
+$ionicConfigProvider.backButton.previousTitleText(true);
+$ionicConfigProvider.views.transition('platform');
+$ionicConfigProvider.navBar.alignTitle('center');
+
+//setup the token interceptor
+$httpProvider.interceptors.push('TokenInterceptor');//adding an interceptor to our array
+
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  //setup the main page
+  .state('main',{
+    url:'/main',
+    templateUrl:'templates/main.html',
+    controller:'MainLMCtrl as lm',
+    cache: false
+  })
+
+  //register state
+  .state('register',{
+        url:'/register',
+        templateUrl:'templates/register.html',
+        controller:'RegisterVMCtrl as vm',
+        cache:false
+      })
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    cache:false,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+//favourites state
+  .state('tab.favourites', {
+      url: '/favourites',
+      cache:false,
+      views: {
+        'tab-favourites': {
+          templateUrl: 'templates/tab-favourites.html',
+          controller: 'FavouritesCtrl'
+        }
+      }
+    })
+
+    .state('tab.chats', {
+          url: '/chats',
+          cache:false,
+          views: {
+            'tab-chats': {
+              templateUrl: 'templates/tab-chats.html',
+              controller: 'ClassCtrl'
+            }
+          }
+        })
+
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'ChatDetailCtrl'
+    //     }
+    //   }
+    // })
+
+    .state('tab.chat-detail', {
+      url: '/chats/:classId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ClassDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl as al'
+      }
+    }
+  })
+
+
+   .state('tab.add', {
+    url: '/add',
+    views: {
+      'tab-add': {
+        templateUrl: 'templates/tab-add.html',
+        controller: 'AccountCtrl as al'
+      }
+    }
+  })
+
+  .state('tab.acount-detail', {
+    url: '/account/:userId',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/account-detail.html',
+        controller: 'AccountDetailCtrl as adl'
+      }
+    }
+  });
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,298 @@
+angular.module('starter.controllers', ['starter.services'])
+
+.controller('DashCtrl', function($scope) {})
+
+//FavouritesCtrl
+.controller('FavouritesCtrl',function($scope,Class,AuthFactory){
+
+  var userId=null;
+  $scope.userloggedIn = null;
+  //implementing listerner when user logged in
+  $scope.$on('GetFavClassesForUser',function(){
+ 
+    //using scope property
+     $scope.favoritesClasses = Class.getFavClass(userId);
+     console.log($scope.favoritesClasses);
+  });
+
+  //getting the user status
+  if(!AuthFactory.isLoggedIn()){
+
+     $scope.userloggedIn=false;
+    //use broadcast on $rootScope for 'showLoginModal'
+  }
+  else{
+    userId= AuthFactory.getUser();
+    $scope.userloggedIn=true;
+    $scope.$broadcast('GetFavClassesForUser'); }//otherwise broadcast
+
+
+})
+
+
+
+//classController
+.controller('ClassCtrl', function($scope, Class,Loader){
+
+ Loader.showLoading("Loading Jobs...");
+  $scope.classes = Class.getClasses(); //asking factory to provide class detail here
+ console.log($scope.classes);
+  $scope.classes.$loaded().then(function(x) {
+    x === $scope.classes; // true
+    Loader.hideLoading();
+  })
+  .catch(function(error) {
+    Loader.hideLoading();
+    console.log("Error:", error);
+  });
+
+
+  console.log("the lenth of array is "+$scope.classes.length);
+})
+
+//classDetail controller
+.controller('ClassDetailCtrl', function($scope,$rootScope,Loader,$stateParams,LSFactory,Class,AuthFactory) {
+  console.log($stateParams.classId);
+  $scope.classDetail = Class.getById($stateParams.classId);
+
+  var userId = AuthFactory.getUser();
+
+  //getting a reference to the fav classes
+  var favClasses = Class.getFavClass(userId);
+  console.log("the favclass lenght is "+favClasses.length);
+
+  //adding listener on our broadcast
+  $scope.$on('addToFavClass', function(){
+
+  Loader.showLoading('Adding to favourites..');
+
+//call back method upon successfully added to favourites
+      var onComplete = function(error) {
+      if (error) {
+        console.log('Synchronization failed');
+      } else {
+        Loader.hideLoading();
+        Loader.toggleLoadingWithMessage('Successfully added ' + $scope.classDetail.title + ' to your favourites', 1500);
+
+        console.log('Synchronization succeeded');
+      }
+    };
+//callback method finish
+
+      var favClassexist = false;
+
+      for(var i=0;i<favClasses.length;i++){
+        if(favClasses[i].id===$scope.classDetail.id){
+          console.log("looprunning");
+          Loader.hideLoading();
+          Loader.toggleLoadingWithMessage('Jobs Already In Your favourites', 1000);
+          console.log("this job already exist");
+          favClassexist=true;
+           break;
+        }
+      }
+
+      //setting only when there's no duplicacy
+    var favRef = new Firebase("https://getjob.firebaseio.com//users/"+userId+"/favClass");
+     if(!favClassexist){
+      favRef.push().set({
+        description: $scope.classDetail.description,
+        id: $scope.classDetail.id,
+        image:$scope.classDetail.image,
+        price:$scope.classDetail.price,
+        title:$scope.classDetail.title
+      },onComplete);
+      }
+
+   });
+
+
+  //adding addToFavClass function on $scope
+  $scope.addToFavClass= function(){
+   if(!AuthFactory.isLoggedIn()){
+     console.log("user not logged in !");
+     Loader.toggleLoadingWithMessage("User not logged in!!",500);
+     //use broadcast on $rootScope for 'showLoginModal'
+   }
+   else{
+     console.log("user logIn with userId: "+ AuthFactory.getUser());
+     $scope.$broadcast('addToFavClass'); }//otherwise broadcast
+  };
+
+
+
+})
+
+
+//AccountCtrl
+.controller('AccountCtrl', function($scope,$state,AuthFactory) {
+
+var al = this;
+al.userId= AuthFactory.getUser();
+   //getting the user status via AuthFactory
+   //getting the user status
+   if(!AuthFactory.isLoggedIn()){
+     console.log("user not logged in ");
+      al.loggedIn=false;
+       console.log(al.userId);
+     //use broadcast on $rootScope for 'showLoginModal'
+   }
+   else{
+     al.loggedIn=true;
+   }
+
+  //doing the logout
+   al.logout= function(){
+    var ref = new Firebase("https://getjob.firebaseio.com/");
+     ref.unauth();
+     AuthFactory.deleteAuth();
+     $state.go('main');
+   };
+
+
+})
+
+//AccountDetailCtrl
+.controller('AccountDetailCtrl',function($scope,$state,Loader,AuthFactory,$firebaseArray){
+
+var adl = this;
+
+
+  var refClass = new Firebase("https://getjob.firebaseio.com/class");
+  var totalClasses = $firebaseArray(refClass);
+  console.log(totalClasses);
+
+  //grabbing data from input field
+
+  //starts----->
+  adl.addClass= function()
+  {
+  Loader.showLoading('Adding Jobs..');
+
+//call back method upon successfully added to favourites
+      var onComplete = function(error) {
+      if (error) {
+        console.log('Synchronization failed');
+      } else {
+        Loader.hideLoading();
+        Loader.toggleLoadingWithMessage('Successfully added ', 1500);
+        adl.classImage="";
+        adl.classTitle="";
+        adl.classBookingPrice="";
+        adl.classDescription="";
+        adl.classId="";
+        $state.go('tab.account');
+
+        console.log('Synchronization succeeded');
+      }
+    };
+//callback method finish
+
+      var favClassexist = false;
+
+      for(var i=0;i<totalClasses.length;i++){
+        if(adl.classId===totalClasses[i].id){
+
+          Loader.hideLoading();
+          Loader.toggleLoadingWithMessage('jobs With id '+adl.classId+' Already Exist ', 1000);
+
+          favClassexist=true;
+           break;
+        }
+      }
+
+      //setting only when there's no duplicacy
+     if(!favClassexist){
+      refClass.push().set({
+        description: adl.classDescription,
+        id: adl.classId,
+        image:adl.classImage,
+        price:adl.classBookingPrice,
+        title:adl.classTitle
+      },onComplete);
+      }
+  };
+
+  //ends--->
+
+
+})
+
+
+.controller('MainLMCtrl', ['$state','AuthFactory','Loader','$rootScope',function($state,AuthFactory,Loader,$rootScope){
+  console.log("I am MainCtrl");
+ var lm = this;
+  //creating a ref to our database
+  var ref = new Firebase("https://getjob.firebaseio.com/");
+
+
+//do some validation here using simple firebase API(without $firebaseAuth)
+  lm.validate=function(){
+      Loader.showLoading('Authenticating...');
+      ref.authWithPassword({
+        email: lm.email,
+        password:lm.password
+         }, function(error, authData) {
+           if (error) {
+              lm.loginError=error.code;
+             console.log("Login Failed!", lm.error);
+             Loader.hideLoading();
+           } else {
+            Loader.hideLoading();
+   console.log("Authenticated successfully with payload:", authData);
+          //getting and saving that into our model
+          $rootScope.name= authData.password.email;
+
+          //setting our user using AuthFactory method
+       AuthFactory.setUser(authData.uid);
+
+          //setting the token here too
+       AuthFactory.setToken(authData.token);
+
+       $state.go('tab.dash');
+
+   }
+     });
+  };
+  /*
+  lm.skip= function(){
+    $state.go('tab.dash');
+  }
+*/
+
+}])
+
+
+//Register Controller
+.controller('RegisterVMCtrl', ["Auth","$firebaseArray","Loader",
+function(Auth,$firebaseArray,Loader){
+  var vm = this;
+   vm.createUser = function(){
+     Loader.showLoading('Registering...');
+     vm.message = null;
+     vm.error= null;
+    // Auth.$createUser().then().catch();
+    Auth.$createUser({
+        email: vm.email,
+        password: vm.password
+      }).then(function(userData) {
+        Loader.hideLoading();
+        console.log(userData);
+        Loader.toggleLoadingWithMessage("User Successfully created");
+        var ref = new Firebase("https://getjob.firebaseio.com/");
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(userData.uid).set({
+      email: vm.email,
+      fullName: "",
+      phone:"",
+      favClass:""
+    });
+
+      }).catch(function(error) {
+        Loader.hideLoading();
+      vm.error = error;
+      });
+   };
+
+}])
